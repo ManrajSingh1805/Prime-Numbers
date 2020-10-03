61f5ec3f0473d1ddeaf186d60ac441e4fba644dc
ManrajSingh1805 <72317410+ManrajSingh1805@users.noreply.github.com>
2020-10-04 00:23:12 +0530

    Add files via upload

---
+++ PM.py
@@ -0,0 +1,23 @@
+fim = input("Enter a number to check whether it's a prime or not:")
+p=None
+y= None
+import sys
+try:
+    f= int(fim)
+except:
+    print("Not a number")
+    sys.exit()
+
+h =f/2
+i=2.00
+while i<h:
+    if f%i==0:
+        p=1
+    else:
+        p=0
+    i=i+1
+
+if p ==0:
+    print("Yes,",f,"is a prime number")
+elif p==1:
+      print("No,",f," is not a prime number")
